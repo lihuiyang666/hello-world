eff0a99a552e568aff0e5d1e0ee74bdf006c9d28
lihuiyang666 <644737330@qq.com>
2020-08-16 22:51:37 +0800

    f commit

---
+++ src/helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld {
+    public static void output(){
+        System.out.println("hello world");
+    }
+    public static void main(String[] args){
+        output();
+
+    }
+}
